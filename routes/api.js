b8a6e750866559fb5dba8f83620e6a4269e98d9b
luisenjs <107270563+luisenjs@users.noreply.github.com>
2024-07-10 09:31:01 -0500

    Finalizando configuración guía 15

--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,8 @@ const itemController = require('../controllers/itemController');
 
 router.post('/items', itemController.createItem);
 router.get('/items', itemController.getAllItems);
+router.get('/items/:id', itemController.getItem);
+router.put('/items/:id', itemController.updateItem);
+router.delete('/items/:id', itemController.deleteItem);
 
 module.exports = router;
